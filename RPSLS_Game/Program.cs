11dffb78d203004bbef2039490f457bccc3f1f11
David <david.chy.usmc@gmail.com>
2018-10-25 11:40:00 -0500

    Declared string and int. Created Player.cs and Game.cs classes.

---
+++ RPSLS_Game/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPlayer, inputCPU;
+            int randomInt;
+            int scorePlayer = 0;
+            int scoreCPU = 0;
+
+            Console.WriteLine("Choose between ROCK, PAPER, SCISSORS, LIZARD, SPOCK:     ");
+            inputPlayer = Console.ReadLine();
+
+            Random rnd = new Random();
+
+            randomInt = rnd.Next(1, 6);
+
+            switch (randomInt)
+            {
+
+            }
+        }
+    }
+}
+
