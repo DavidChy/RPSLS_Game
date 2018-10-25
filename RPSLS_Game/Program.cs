413b97afe260664be62dcf9459297c3e49c478dd
David <david.chy.usmc@gmail.com>
2018-10-25 14:27:56 -0500

    Work In Progress: Switch statement for ROCK, PAPER, SCISSORS.

--- RPSLS_Game/Program.cs
+++ RPSLS_Game/Program.cs
@@ -24,7 +24,63 @@ namespace RPSLS_Game
 
             switch (randomInt)
             {
-
+                case 1:
+                    inputCPU = "ROCK";
+                    Console.WriteLine("Computer chose ROCK");
+                    if (inputPlayer == "ROCK")
+                    {
+                        Console.WriteLine("DRAW!!\n\n");
+                    }
+                    else if (inputPlayer == "PAPER")
+                    {
+                        Console.WriteLine("PLAYER WINS!!\n\n");
+                        scorePlayer++;
+                    }
+                    else if (inputPlayer == "SCISSORS")
+                    {
+                        Console.WriteLine("CPU WINS!!\n\n");
+                        scoreCPU++;
+                    }
+                    break;
+                case 2:
+                    inputCPU = "PAPER";
+                    Console.WriteLine("Computer chose PAPER");
+                    if (inputPlayer == "PAPER")
+                    {
+                        Console.WriteLine("DRAW!!\n\n");
+                    }
+                    else if (inputPlayer == "ROCK")
+                    {
+                        Console.WriteLine("CPU WINS!!\n\n");
+                        scoreCPU++;
+                    }
+                    else if (inputPlayer == "SCISSORS")
+                    {
+                        Console.WriteLine("PLAYER WINS!!\n\n");
+                        scorePlayer++;
+                    }
+                    break;
+                case 3:
+                    inputCPU = "SCISSORS";
+                    Console.WriteLine("Computer chose SCISSORS");
+                    if (inputPlayer == "SCISSORS")
+                    {
+                        Console.WriteLine("DRAW!!\n\n");
+                    }
+                    else if (inputPlayer == "ROCK")
+                    {
+                        Console.WriteLine("PLAYER WINS!!\n\n");
+                        scorePlayer++;
+                    }
+                    else if (inputPlayer == "PAPER")
+                    {
+                        Console.WriteLine("CPU WINS!!\n\n");
+                        scoreCPU++;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid entry!");
+                    break;
             }
         }
     }
