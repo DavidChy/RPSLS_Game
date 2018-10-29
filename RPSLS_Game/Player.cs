2b83763908bc183fdc135bf50947853ca804ec58
David <david.chy.usmc@gmail.com>
2018-10-29 07:51:46 -0500

    Removed scoreboard class. Added Human class and computer class. Replaced switch statements with if else statements instead. Reorganized code and cleaned up errors.

--- RPSLS_Game/Player.cs
+++ RPSLS_Game/Player.cs
@@ -8,5 +8,27 @@ namespace RPSLS_Game
 {
     class Player
     {
+        public string name;
+        public string choice;
+        public int score = 0;
+
+
+        public virtual void MakeChoice()
+        {
+            Console.WriteLine(name + ", choose rock, paper, scissors, lizard, or spock.");
+            choice = Console.ReadLine();
+        }
+        public void AddToScore()
+        {
+            score += 1;
+        }
+        public int GetScore()
+        {
+            return score;
+        }
+        public virtual void SetName()
+        {
+
+        }
     }
 }
